32e3ef55281103d22010cbcd41f96d8491ae7f97
Wesley Moraes Benatti <wesleymbenatti@gmail.com>
2021-03-14 16:55:54 -0300

    Métodos Subtotal e total

--- src/main/java/com/example/webService/entities/Order.java
+++ src/main/java/com/example/webService/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem item : items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
